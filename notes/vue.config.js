8a3819c26f053c03d34f4c08e2280fc193e3649f
TursunboyevJahongir <jah6332@gmail.com>
2021-05-17 10:59:05 +0500

    install vue-cli

---
+++ notes/vue.config.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+ module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : '/',
+    assetsDir:'assets'
+  }
